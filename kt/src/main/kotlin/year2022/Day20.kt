7caf7162285916eb3bae600498c8ddf8665fc10e
Phil Davies <git@karn.co.uk>
2022-12-20 16:49:20 +0000

    [kt] day 20 tidied more

--- kt/src/main/kotlin/year2022/Day20.kt
+++ kt/src/main/kotlin/year2022/Day20.kt
@@ -5,33 +5,16 @@ import aok.PuzzleInput
 import aoksp.AoKSolution
 import queryPuzzles
 import solveAll
+import kotlin.math.absoluteValue
 
 fun main(): Unit = with(InputScopeProvider) {
     queryPuzzles { year == 2022 && day == 20 }.solveAll(
-        warmupIterations = 30, runIterations = 3
+        warmupIterations = 25, runIterations = 1
     )
 }
 
 @AoKSolution
 object Day20 {
-    context(PuzzleInput)
-    private fun parse() = lines.map(String::toInt).let { ints ->
-        val end = Node(ints.last())
-        ints.asReversed().drop(1).fold(end) { tail, v ->
-            Node(v, tail).also {
-                it.right = tail
-                tail.left = it
-            }
-        }.also {
-            it.left = end
-            end.right = it
-        }
-    }
-
-    class Node(val value: Int, val next: Node? = null) {
-        lateinit var left: Node
-        lateinit var right: Node
-    }
 
     context(PuzzleInput)
     fun part1() = parse().decryptCoordinates()
@@ -39,44 +22,65 @@ object Day20 {
     context(PuzzleInput)
     fun part2() = parse().decryptCoordinates(811589153, 10)
 
-    private fun Node.decryptCoordinates(decryptionKey: Long = 1, rounds: Int = 1): Long {
+    context(PuzzleInput)
+    private fun parse() = lines.map(String::toInt).map(::Node).also {
+        it.first().left = it.last()
+        it.last().right = it.first()
+        it.zipWithNext { a, b ->
+            a.right = b
+            b.left = a
+        }
+    }
+
+    private fun List<Node>.decryptCoordinates(decryptionKey: Long = 1, rounds: Int = 1): Long {
         val zero = mix(decryptionKey, rounds)
         return generateSequence(zero, Node::right).map(Node::value)
             .filterIndexed { index, _ -> index % 1000 == 0 }
             .drop(1).take(3).sum() * decryptionKey
     }
 
-    private fun Node.mix(decryptionKey: Long = 1, rounds: Int = 1): Node {
+    private fun List<Node>.mix(decryptionKey: Long = 1, rounds: Int = 1): Node {
         lateinit var zero: Node
-        val n = generateSequence(this, Node::next).count() - 1 // node is removed during mod
-        val keyMod = decryptionKey.mod(n)
-        // ab mod n == (a mod n)(b mod n) mod n
+        val w = lastIndex / 2
+        val modKey = decryptionKey.mod(lastIndex)
         repeat(rounds) {
-            var cur = this
-            while (true) {
+            for (cur in this) {
                 if (cur.value == 0) {
                     zero = cur
                 } else {
-                    val moves = (keyMod * cur.value.mod(n)).mod(n)
+                    // ab mod n == (a mod n)(b mod n) mod n
+                    val moves = (modKey * cur.value.mod(lastIndex)).mod(lastIndex)
                     if (moves > 0) {
-                        var dest = cur
-                        repeat(moves) {
-                            dest = dest.right
-                        }
-                        // unlink current
-                        cur.left.right = cur.right
-                        cur.right.left = cur.left
-
-                        // link at dest
-                        cur.left = dest
-                        cur.right = dest.right
-                        cur.left.right = cur
-                        cur.right.left = cur
+                        val dest = cur.traverse(if (moves < w) moves else moves - size)
+                        cur moveTo dest
                     }
                 }
-                cur = cur.next ?: break
             }
         }
         return zero
+    }
+
+    class Node(val value: Int) {
+        lateinit var left: Node
+        lateinit var right: Node
+    }
+
+    private fun Node.traverse(n: Int): Node {
+        var node = this
+        if (n > 0) repeat(n) { node = node.right }
+        else repeat(-n) { node = node.left }
+        return node
+    }
+
+    private infix fun Node.moveTo(dest: Node) {
+        // unlink current
+        left.right = right
+        right.left = left
+
+        // link at dest
+        left = dest
+        right = dest.right
+        left.right = this
+        right.left = this
     }
 }
