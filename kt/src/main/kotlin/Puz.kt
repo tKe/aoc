9b2b1c166636b9118afd4263671e2fd3584eb646
Phil Davies <git@karn.co.uk>
2022-12-20 19:52:59 +0000

    [kt] warmup by duration

--- kt/src/main/kotlin/Puz.kt
+++ kt/src/main/kotlin/Puz.kt
@@ -1,8 +1,11 @@
+import aok.PuzzleInput
+import kotlinx.coroutines.withTimeout
 import year2022.Day07JimFS
 import year2022.Puz22
 import year2022.AoKYear2022
 import kotlin.reflect.KClass
 import kotlin.time.Duration
+import kotlin.time.TimeSource
 import kotlin.time.TimedValue
 import kotlin.time.measureTime
 import kotlin.time.measureTimedValue
@@ -31,6 +34,57 @@ private val Puz<*, *>.defaultVariant
         ?.takeUnless(String::isBlank)
         ?: "Default"
 
+sealed interface Warmup {
+    context(PuzzleInput)
+    fun run(year: Int, day: Int, puzzles: List<Puz<*, *>>)
+
+    class Iterations(val warmupIterations: Int) : Warmup {
+        context(PuzzleInput) override fun run(year: Int, day: Int, puzzles: List<Puz<*, *>>) {
+            println("Warming up ${puzzles.size} puzzles $warmupIterations times for year $year day $day...")
+            measureTime {
+                repeat(warmupIterations) {
+                    puzzles.forEach {
+                        runCatching { it.part1() }
+                        runCatching { it.part2() }
+                    }
+                }
+            }.also { println("year $year day $day warmup ($warmupIterations iterations) took $it") }
+        }
+    }
+
+    class DurationTotal(val d: Duration) : Warmup {
+        context(PuzzleInput) override fun run(year: Int, day: Int, puzzles: List<Puz<*, *>>) {
+            val start = TimeSource.Monotonic.markNow()
+            println("Warming up ${puzzles.size} puzzles over $d for year $year day $day...")
+            var count = 0
+            while (start.elapsedNow() < d) {
+                puzzles.forEach {
+                    runCatching { it.part1() }
+                    runCatching { it.part2() }
+                }
+                count++
+            }
+            println("Warmup finished after ${start.elapsedNow()} with $count iterations")
+        }
+    }
+
+    class DurationPerSolution(val d: Duration) : Warmup {
+        context(PuzzleInput) override fun run(year: Int, day: Int, puzzles: List<Puz<*, *>>) {
+            println("Warming up ${puzzles.size} puzzles for $d each for year $year day $day...")
+            puzzles.forEach {
+                val start = TimeSource.Monotonic.markNow()
+                var count = 0
+                while (start.elapsedNow() < d) {
+                    runCatching { it.part1() }
+                    runCatching { it.part2() }
+                    count++
+                }
+                println("\t${it.variant} warmed up with $count iterations")
+            }
+        }
+    }
+}
+
 private fun <T : Any> sealedObjects(kClass: KClass<out T>): Iterable<T> =
     (kClass.objectInstance?.let { listOf(it) } ?: emptyList()) + kClass.sealedSubclasses
         .flatMap(::sealedObjects)
@@ -47,21 +101,32 @@ inline fun <reified T : Puz<*, *>> solveAll(
     runIterations: Int = 1,
 ) = getAll<T, _, _>().solveAll(warmupIterations = warmupIterations, runIterations = runIterations)
 
+
 context(InputScopeProvider)
-fun Iterable<Puz<*, *>>.solveAll(warmupIterations: Int = 0, runIterations: Int = 1) =
+fun Iterable<Puz<*, *>>.warmup(warmup: Warmup) = apply {
     groupBy { it.year to it.day }.forEach { (year, day), puzzles ->
         with(forPuzzle(year, day, "input.txt")) {
-            if (warmupIterations > 0) {
-                measureTime {
-                    repeat(warmupIterations) {
-                        puzzles.forEach {
-                            runCatching { it.part1() }
-                            runCatching { it.part2() }
-                        }
-                    }
-                }.also { println("year $year day $day warmup ($warmupIterations iterations) took $it") }
-            }
+            warmup.run(year, day, puzzles)
+        }
+    }
+}
 
+context(InputScopeProvider)
+fun Iterable<Puz<*, *>>.warmup(iterations: Int) = warmup(Warmup.Iterations(iterations))
+
+context(InputScopeProvider)
+fun Iterable<Puz<*, *>>.warmup(duration: Duration) = warmup(Warmup.DurationTotal(duration))
+
+context(InputScopeProvider)
+fun Iterable<Puz<*, *>>.warmupEach(duration: Duration) = warmup(Warmup.DurationPerSolution(duration))
+
+context(InputScopeProvider)
+fun Iterable<Puz<*, *>>.solveAll(warmupIterations: Int, runIterations: Int = 1) = warmup(warmupIterations).solveAll(runIterations)
+
+context(InputScopeProvider)
+fun Iterable<Puz<*, *>>.solveAll(runIterations: Int = 1) =
+    groupBy { it.year to it.day }.forEach { (year, day), puzzles ->
+        with(forPuzzle(year, day, "input.txt")) {
             fun runPart(part: Puz<*, *>.() -> Any?) {
                 val results = puzzles.map { puz ->
                     puz.variant to runCatching {
