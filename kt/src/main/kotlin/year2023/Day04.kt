143627d4251a81a86d210165d0ecc7ea77bc78bc
tKe <git@karn.co.uk>
2023-12-04 07:55:24 +0000

    [kt] 2023 day 4

---
+++ kt/src/main/kotlin/year2023/Day04.kt
@@ -0,0 +1,28 @@
+package year2023
+
+import aok.PuzDSL
+import aoksp.AoKSolution
+import utils.splitIntsNotNull
+
+fun main() = solveDay(
+    4,
+//    input = aok.InputProvider.Example
+)
+
+@AoKSolution
+object Day04 : PuzDSL({
+    val parseWins = lineParser { line ->
+        line.split(" | ", limit = 2)
+            .map { it.splitIntsNotNull(" ").toSet() }
+            .let { (winners, numbers) -> numbers.count(winners::contains) }
+    }
+    part1(parseWins) { wins ->
+        wins.sumOf { 1.shl(it - 1) }
+    }
+
+    part2(parseWins) { wins ->
+        val copies = IntArray(wins.size) { 1 }
+        for (idx in wins.indices) repeat(wins[idx]) { copies[it + idx + 1]+=copies[idx] }
+        copies.sum()
+    }
+})
