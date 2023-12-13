5b1f5dbd49876f4e668c145db0e6b5f559ed233e
tKe <git@karn.co.uk>
2023-12-13 09:35:06 +0000

    refactor: [kt] 2023 day 13

--- kt/src/main/kotlin/year2023/Day13.kt
+++ kt/src/main/kotlin/year2023/Day13.kt
@@ -5,39 +5,29 @@ import aoksp.AoKSolution
 
 fun main() = solveDay(
     13,
+    warmup = aok.Warmup.iterations(300), runs = 10
 )
 
 @AoKSolution
 object Day13 : PuzDSL({
+    val parser = parser { input.split("\n\n").map { it.lines() } }
+
     fun List<String>.transposed() = first().indices.map {
         buildString { for (line in this@transposed) append(line[it]) }
     }
 
-    fun List<String>.reflection() = zipWithNext(String::equals)
-        .mapIndexedNotNull { idx, b -> idx.takeIf { b }?.inc() }
-        .firstOrNull { lines ->
-            take(lines).reversed().zip(drop(lines)) { a, b -> a == b }.all { it }
-        } ?: 0
+    fun List<String>.reflection() = (1..lastIndex).firstOrNull { lines ->
+        take(lines).reversed().zip(drop(lines)) { a, b -> a == b }.all { it }
+    } ?: 0
 
-    part1 {
-        val grids = input.split("\n\n").map { it.lines() }
-        grids.sumOf {
-            100 * it.reflection() + it.transposed().reflection()
-        }
-    }
+    fun List<List<String>>.summarizeBy(f: List<String>.() -> Int) = sumOf { 100 * it.f() + it.transposed().f() }
+
+    part1(parser) { it.summarizeBy { reflection() } }
 
     fun String.smudges(other: String) = zip(other) { a, b -> a != b }.count { it }
-    fun List<String>.smudgeReflection() = zipWithNext(String::smudges)
-        .mapIndexedNotNull { idx, i -> idx.takeIf { i <= 1 }?.inc() }
-        .firstOrNull { lines ->
-            take(lines).reversed().zip(drop(lines), String::smudges).sum() == 1
-        } ?: 0
+    fun List<String>.smudgeReflection() = (1..lastIndex).firstOrNull { lines ->
+        take(lines).reversed().zip(drop(lines), String::smudges).sum() == 1
+    } ?: 0
 
-
-    part2 {
-        val grids = input.split("\n\n").map { it.lines() }
-        grids.sumOf {
-            100 * it.smudgeReflection() + it.transposed().smudgeReflection()
-        }
-    }
+    part2(parser) { it.summarizeBy { smudgeReflection() } }
 })
