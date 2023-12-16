676c6249cfd91ad81d952f13e1fade5c43cf14df
tKe <git@karn.co.uk>
2023-12-16 23:46:50 +0000

    [kt] 2023 day 16 improve.

--- kt/src/main/kotlin/year2023/Day16.kt
+++ kt/src/main/kotlin/year2023/Day16.kt
@@ -3,34 +3,38 @@ package year2023
 import aok.PuzDSL
 import aoksp.AoKSolution
 import year2023.Day16.Direction.*
+import kotlin.experimental.and
+import kotlin.experimental.or
 
 fun main() = solveDay(16)
 
 @AoKSolution
 object Day16 : PuzDSL({
     fun List<String>.tile(beam: Beam) = getOrNull(beam.y)?.getOrNull(beam.x)
-    fun List<String>.cast(start: Beam = Beam(0, 0, Right), onSplit: (Beam) -> Unit) = sequence {
-        var beam = start
-        val visited = mutableSetOf<Beam>()
-        while (true) {
-            val tile = tile(beam) ?: break
-            if (!visited.add(beam)) break
-            yield(beam.x to beam.y)
-            beam = beam.handle(tile).also { (_, split) ->
-                split?.let { onSplit(it) }
-            }.first
-        }
-    }
 
-    fun List<String>.energize(
-        start: Beam = Beam(0, 0, Right),
-    ): Int = buildSet {
-        val beams = mutableSetOf(start)
-        val pending = ArrayDeque(beams)
-        while (pending.isNotEmpty()) {
-            addAll(cast(pending.removeFirst()) { if (beams.add(it)) pending += it })
+    fun List<String>.energize(start: Beam = Beam(0, 0, Right)): Int = with(ByteArray(size * first().length)) {
+        fun visited(beam: Beam) = (beam.x * this@energize.size + beam.y).let { idx ->
+            if (idx in indices) {
+                val cur = this[idx]
+                val bit = 1.shl(beam.d.ordinal).toByte()
+                (cur and bit == bit).also { this[idx] = cur or bit }
+            } else true
         }
-    }.count()
+
+        val pending = ArrayDeque(listOf(start))
+        while (pending.isNotEmpty()) {
+            var beam = pending.removeFirst()
+            while (true) {
+                val tile = tile(beam) ?: break
+                if (visited(beam)) break
+                beam = if (tile == '.') beam.move()
+                else beam.handle(tile).also { (_, split) ->
+                    split?.let { pending += it }
+                }.first
+            }
+        }
+        count { it > 0 }
+    }
 
     part1 {
         lines.energize()
@@ -38,17 +42,17 @@ object Day16 : PuzDSL({
 
     part2 {
         sequence {
-            val yr = lines.indices
-            val xr = lines.first().indices
-            for (y in yr) {
-                yield(lines.energize(Beam(xr.first, y, Right)))
-                yield(lines.energize(Beam(xr.last, y, Left)))
+            val lastY = lines.lastIndex
+            val lastX = lines.first().lastIndex
+            for (y in 0..lastY) {
+                yield(Beam(0, y, Right))
+                yield(Beam(lastX, y, Left))
             }
-            for (x in xr) {
-                yield(lines.energize(Beam(x, yr.first, Down)))
-                yield(lines.energize(Beam(x, yr.last, Up)))
+            for (x in 0..lastX) {
+                yield(Beam(x, 0, Down))
+                yield(Beam(x, lastY, Up))
             }
-        }.max()
+        }.maxOf(lines::energize)
     }
 }) {
     enum class Direction {
