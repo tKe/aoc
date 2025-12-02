b98391bdaa153d601e4e0b859623f5ca30b8dd74
Phil Davies <git@karn.co.uk>
2025-12-02 14:12:12 +0000

    [kt] y25d02 alternate

--- kt/src/main/kotlin/year2025/Day02.kt
+++ kt/src/main/kotlin/year2025/Day02.kt
@@ -1,9 +1,17 @@
 package year2025
 
+import aok.Parser
 import aok.PuzDSL
+import aok.PuzzleInput
+import aok.Warmup
+import aok.input
 import aoksp.AoKSolution
+import utils.splitLongs
+import kotlin.math.log10
+import kotlin.math.pow
+import kotlin.time.Duration.Companion.seconds
 
-@AoKSolution
+//@AoKSolution
 object Day02 : PuzDSL({
     part1 {
         input.split(",").sumOf {
@@ -25,4 +33,63 @@ object Day02 : PuzDSL({
     }
 })
 
-fun main() = solveDay(2)//, input = InputProvider.raw("""1188511880-1188511890"""))
+@AoKSolution
+object Day02Regex : PuzDSL({
+    context(_: PuzzleInput)
+    fun solve(regex: Regex) = input.splitLongs(',', '-')
+        .chunked(2) { (start, end) -> (start..end) }
+        .sumOf { r -> r.sumOf { if ("$it".matches(regex)) it else 0 } }
+    part1 { solve("""(\d+)\1""".toRegex()) }
+    part2 { solve("""(\d+)\1+""".toRegex()) }
+})
+
+@AoKSolution
+object Day02ChunkGeneration : PuzDSL({
+    fun Long.digits(): Int = log10(toDouble()).inc().toInt()
+    val pow10 = LongArray(12) { 10.0.pow(it).toLong() }
+
+    fun LongRange.sumInvalidIds(maxChunks: Int = endInclusive.digits()): Long {
+        val seen = mutableSetOf<Long>()
+        var sum = 0L
+
+        fun visitRange(start: Long, end: Long) {
+            val len = start.digits()
+            for (chunks in 2..maxChunks) {
+                if (len % chunks != 0) continue
+                val chunkLen = len / chunks
+                val chunkShift = pow10[chunkLen]
+                val prefixShift = pow10[len - chunkLen]
+
+                val startPrefix = start / prefixShift
+                val endPrefix = end / prefixShift
+                for (chunk in startPrefix..endPrefix) {
+                    var invalid = chunk
+                    repeat(chunks - 1) {
+                        invalid *= chunkShift
+                        invalid += chunk
+                    }
+                    if (invalid in start..end && seen.add(invalid)) sum += invalid
+                }
+            }
+        }
+
+        var start = start
+        while (start < endInclusive) {
+            val end = pow10[start.digits()]
+            if (end <= endInclusive) visitRange(start, end - 1)
+            else visitRange(start, endInclusive)
+            start = end
+        }
+        return sum
+    }
+
+    val ranges = Parser {
+        input.split(",", "-")
+            .chunked(2) { (start, end) -> (start.toLong()..end.toLong()) }
+    }
+
+    part1(ranges) { ranges -> ranges.sumOf { it.sumInvalidIds(maxChunks = 2) } }
+    part2(ranges) { ranges -> ranges.sumOf { it.sumInvalidIds() } }
+})
+
+fun main() = solveDay(2, warmup = Warmup.eachFor(5.seconds), runs=500)
